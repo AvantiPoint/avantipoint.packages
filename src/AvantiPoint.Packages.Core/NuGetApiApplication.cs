ccad7e868552d6baa82014ba529ffc935e516a7e
Dan Siegel <me@dansiegel.net>
2021-04-21 10:39:17 -0700

    add environment information to NuGetApiApplication

--- src/AvantiPoint.Packages.Core/NuGetApiApplication.cs
+++ src/AvantiPoint.Packages.Core/NuGetApiApplication.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AvantiPoint.Packages.Core
@@ -8,8 +9,15 @@ namespace AvantiPoint.Packages.Core
         public NuGetApiApplication(IServiceCollection services)
         {
             Services = services ?? throw new ArgumentNullException(nameof(services));
+
+            var hostEnvironment = services.FirstOrDefault(x => x.ServiceType.FullName == "Microsoft.Extensions.Hosting.IHostEnvironment")?.ImplementationInstance;
+            EnvironmentName = (string)hostEnvironment.GetType().GetProperty("EnvironmentName").GetValue(hostEnvironment);
         }
 
         public IServiceCollection Services { get; }
+
+        public string EnvironmentName { get; }
+
+        public bool IsDevelopment => EnvironmentName.Equals("Development", StringComparison.OrdinalIgnoreCase);
     }
 }
