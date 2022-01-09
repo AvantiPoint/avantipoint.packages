9a34a18662fe6b5f017c45915a54c0763fbf00ec
Dan Siegel <me@dansiegel.net>
2022-01-09 10:07:54 -0800

    use System.Text.Json

--- src/AvantiPoint.Packages.Core/Entities/Converters/StringArrayToJsonConverter.cs
+++ src/AvantiPoint.Packages.Core/Entities/Converters/StringArrayToJsonConverter.cs
@@ -1,17 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Newtonsoft.Json;
 
 namespace AvantiPoint.Packages.Core
 {
     public class StringArrayToJsonConverter : ValueConverter<string[], string>
     {
-        public static readonly StringArrayToJsonConverter Instance = new StringArrayToJsonConverter();
+        public static readonly StringArrayToJsonConverter Instance = new ();
 
         public StringArrayToJsonConverter()
             : base(
-                v => JsonConvert.SerializeObject(v),
-                v => (!string.IsNullOrEmpty(v)) ? JsonConvert.DeserializeObject<string[]>(v) : new string[0])
+                v => Serialize(v),
+                v => Deserialize(v))
         {
+        }
+
+        private static string Serialize(IEnumerable<string> input)
+        {
+            if(input is null)
+                input = Array.Empty<string>();
+
+            input = input.Where(x => !string.IsNullOrEmpty(x))
+                .Select(x => x.Trim());
+
+            return JsonSerializer.Serialize(input);
+        }
+
+        private static string[] Deserialize(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return Array.Empty<string>();
+
+            try
+            {
+                return JsonSerializer.Deserialize<string[]>(json);
+            }
+            catch
+            {
+                return Array.Empty<string>();
+            }
         }
     }
 }
