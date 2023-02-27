402b06d508903b67025b7213b1a09b6109ccf162
Dan Siegel <me@dansiegel.net>
2023-02-27 11:50:11 -0600

    chore: update samples

--- samples/AuthenticatedFeed/Program.cs
+++ samples/AuthenticatedFeed/Program.cs
@@ -1,8 +1,40 @@
-using AuthenticatedFeed;
+using AuthenticatedFeed.Services;
+using AvantiPoint.Packages;
+using AvantiPoint.Packages.Core;
+using AvantiPoint.Packages.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-Startup.ConfigureServices(builder.Services);
+builder.Services.AddScoped<IPackageAuthenticationService, DemoNuGetAuthenticationService>()
+    .AddScoped<INuGetFeedActionHandler, DemoActionHandler>()
+    .AddNuGetPackageApi(app =>
+    {
+        app.AddFileStorage()
+           //.AddUpstreamSource("NuGet.org", "https://api.nuget.org/v3/index.json")
+           .AddSqlServerDatabase("DefaultConnection");
+    });
+
 var app = builder.Build();
-Startup.Configure(app, app.Environment);
+if (app.Environment.IsDevelopment())
+{
+#if DEBUG
+    using var scope = app.Services.CreateScope();
+    using var db = scope.ServiceProvider.GetRequiredService<IContext>();
+    db.Database.EnsureCreated();
+#endif
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseOperationCancelledMiddleware();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapNuGetApiRoutes();
+});
+
 await app.RunAsync();
--- samples/OpenFeed/Program.cs
+++ samples/OpenFeed/Program.cs
@@ -1,8 +1,51 @@
+using AvantiPoint.Packages;
+using AvantiPoint.Packages.Core;
+using AvantiPoint.Packages.Hosting;
 using Microsoft.AspNetCore.Builder;
-using OpenFeed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-Startup.ConfigureServices(builder.Services);
+builder.Services.AddNuGetPackageApi(options =>
+{
+    options.AddFileStorage();
+    //.AddUpstreamSource("NuGet.org", "https://api.nuget.org/v3/index.json")
+
+    switch (options.Options.Database.Type)
+    {
+        case "SqlServer":
+            options.AddSqlServerDatabase("SqlServer");
+            break;
+        case "MariaDb":
+            options.AddMariaDb("MariaDb");
+            break;
+        case "MySql":
+            options.AddMySqlDatabase("MySql");
+            break;
+        default:
+            options.AddSqliteDatabase("Sqlite");
+            break;
+    }
+});
 var app = builder.Build();
-Startup.Configure(app, app.Environment);
+
+if (app.Environment.IsDevelopment())
+{
+#if DEBUG
+    using var scope = app.Services.CreateScope();
+    using var db = scope.ServiceProvider.GetRequiredService<IContext>();
+    db.Database.EnsureCreated();
+#endif
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseOperationCancelledMiddleware();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapNuGetApiRoutes();
+});
 await app.RunAsync();
